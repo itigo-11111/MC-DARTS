c2397aea6dc914b97fdce045198b3b99672b81ce
itigo-11111 <38446219+itigo-11111@users.noreply.github.com>
2022-11-08 11:17:41 +0900

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -78,28 +78,6 @@ AmoebaNet = Genotype(
   reduce_concat = [3, 4, 6]
 )
 
-MyNet = Genotype(
-  normal=[
-    ('sep_conv_3x3', 0), 
-    ('sep_conv_3x3', 1), 
-    ('sep_conv_3x3', 0), 
-    ('skip_connect', 2), 
-    ('skip_connect', 2), 
-    ('sep_conv_3x3', 0), 
-    ('sep_conv_3x3', 0), 
-    ('sep_conv_3x3', 1)], 
-  normal_concat=range(2, 6), 
-  reduce=[('max_pool_3x3', 0),
-    ('sep_conv_5x5', 1),
-    ('dil_conv_3x3', 2), 
-    ('sep_conv_3x3', 1), 
-    ('sep_conv_5x5', 2), 
-    ('sep_conv_5x5', 1), 
-    ('sep_conv_5x5', 1), 
-    ('sep_conv_3x3', 0)], 
-  reduce_concat=range(2, 6)
-)
-
 DARTS_V1 = Genotype(normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 0), ('sep_conv_3x3', 1), ('skip_connect', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 0), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('avg_pool_3x3', 0)], reduce_concat=[2, 3, 4, 5])
 DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 1), ('skip_connect', 0), ('skip_connect', 0), ('dil_conv_3x3', 2)], normal_concat=[2, 3, 4, 5], reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)], reduce_concat=[2, 3, 4, 5])
 
