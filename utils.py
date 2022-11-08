44074d0aee41e41fe5ca334b43e54ad75f0ee3bc
itigo-11111 <38446219+itigo-11111@users.noreply.github.com>
2022-11-08 14:45:40 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,3 @@
-try:
-    from torch.hub import load_state_dict_from_url
-except ImportError:
-    from torch.utils.model_zoo import load_url as load_state_dict_from_url
-
 import os
 import numpy as np
 import torch
